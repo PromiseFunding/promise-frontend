c82a6bfb51530b0cceff6f86910e94b1d42921c6
Silas Lenihan <sjl58@duke.edu>
2022-09-23 12:54:08 -0400

    started cloud functions

--- cloudFunctions/updateFunders.js
+++ cloudFunctions/updateFunders.js
@@ -1,5 +1,32 @@
 Moralis.Cloud.afterSave("FunderAdded", async (request) => {
     const confirmed = request.object.get("confirmed")
     const logger = Moralis.Cloud.getLogger()
-    logger.info("Funder Added :D")
+    if (confirmed) {
+        logger.info("Funder Added :D")
+        const ActiveItem = Moralis.Object.extend("ActiveFunder")
+
+        const query = new Moralis.Query(ActiveItem)
+        query.equalTo("funder", request.object.get("funder"))
+        query.equalTo("owner", request.object.get("owner"))
+        query.equalTo("assetAddress", request.object.get("assetAddress"))
+
+        const alreadyListedItem = await query.first()
+        if (alreadyListedItem) {
+            log("")
+            log("hey!", alreadyListedItem.get("amount"))
+        }
+
+        const activeItem = new ActiveItem()
+        activeItem.set("funder", request.object.get("funder"))
+        activeItem.set("owner", request.object.get("owner"))
+        activeItem.set("assetAddress", request.object.get("assetAddress"))
+        activeItem.set("amount", request.object.get("amount"))
+        logger.info(
+            `Adding Address: ${request.object.get("address")}. TokenId: ${request.object.get(
+                "tokenId"
+            )}`
+        )
+        logger.info("Saving...")
+        await activeItem.save()
+    }
 })
