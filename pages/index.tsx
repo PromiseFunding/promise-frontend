062c7f19cf4b0dc207838fd9c7cfd2410bf03fef
Dylan <dhp14@duke.edu>
2022-10-05 01:35:17 -0400

    cleaned up home page added details page and added straight donation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 import Header from "../components/Header"
-import Deposit from "../components/Deposit"
-import Withdraw from "../components/WithdrawFunder"
-import WithdrawProceeds from "../components/WithdrawProceeds"
-import PoolInfo from "../components/PoolInfo"
+import * as React from "react"
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import Typography from "@mui/material/Typography"
+import { CardActionArea } from "@mui/material"
 
 const Home: NextPage = () => {
     return (
@@ -16,14 +18,24 @@ const Home: NextPage = () => {
                 <meta name="description" content="Version one of the FundMe Smart Contract" />
             </Head>
             <Header></Header>
-            <div className="p-5 flex flex-row">
-                <Deposit></Deposit>
-                <Withdraw></Withdraw>
-                <WithdrawProceeds></WithdrawProceeds>
-            </div>
+            <br></br>
             <div>
-                <PoolInfo></PoolInfo>
+            <h1 className="font-blog text-4xl text-slate-200">Discover Fundraisers</h1>
             </div>
+            <br></br>
+            <br></br>
+            <Card sx={{ maxWidth: 345, minHeight: 250 }}>
+                <CardActionArea href="/details">
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            First Fundraiser
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Proof of Concept for Donating to projects you believe in. Our project is YieldMe!
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
         </div>
     )
 }
