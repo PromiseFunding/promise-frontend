ebaca310ea403f3db9309042675e0565781a87a4
Silas Lenihan <sjl58@duke.edu>
2022-10-05 12:03:08 -0400

    small style changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,12 +15,12 @@ export default function Header() {
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div className="text-lg lg:flex-grow">
                     <Link href="/">
-                        <a className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 bg-slate-800 hover:text-white mr-4 rounded px-5 py-2">
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 bg-slate-800 hover:text-white mr-4 rounded px-5 py-2 hover:bg-blue-700">
                             Home
                         </a>
                     </Link>
                     <Link href="/form">
-                        <a className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 bg-slate-800 hover:text-white mr-4 rounded px-5 py-2">
+                        <a className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 bg-slate-800 hover:text-white mr-4 rounded px-5 py-2 hover:bg-blue-700">
                             Create New Fund
                         </a>
                     </Link>
