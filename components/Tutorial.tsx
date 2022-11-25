b3a47ca9c32743fac48336bbf19562aab60ec327
Silas Lenihan <sjl58@duke.edu>
2022-11-25 13:37:20 -0600

    Added images and started Tutorial componenet

---
+++ components/Tutorial.tsx
@@ -0,0 +1,15 @@
+import styles from "../styles/Home.module.css"
+
+export default function Tutorial() {
+
+    return (
+        <div>
+            <div className={styles.tutorialPanel}>
+                Tutorial
+            </div>
+            <a href="http://www.freepik.com">Designed by vectorjuice / Freepik</a>
+
+        </div>
+
+    )
+}
