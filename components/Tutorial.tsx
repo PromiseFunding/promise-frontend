5a5710df7e589b071868e06aa41e71bc508ee386
Silas Lenihan <sjl58@duke.edu>
2022-11-29 13:54:55 -0500

    started how it works page

--- components/Tutorial.tsx
+++ components/Tutorial.tsx
@@ -7,23 +7,39 @@ export default function Tutorial() {
         <div style={{ display: "flex", flexDirection: "column" }}>
             <div className={styles.tutorialPanel}>
                 <div style={{
-                    textAlign: "center", width: "40%"
+                    textAlign: "center", width: "50%"
                 }}>
                     <Image src="/../public/info-images/idea/idea.png" height="800" width="800" alt="image"></Image>
                 </div>
                 <div className={styles.tutorialText} >
-                    IDEA
+                    <div>
+                        <h1 style={{ fontSize: "60px", fontWeight: "700" }}>It starts with an idea</h1>
+                        <h2 style={{ fontSize: "40px" }}>Sitting on a new project idea? Maybe...</h2>
+                        <ul style={{ marginTop: "10px", fontSize: "20px" }}>
+                            <li>A New DeFi App?</li>
+                            <li>An Artistic Venture?</li>
+                            <li>A Revolutionary Product?</li>
+                        </ul>
+
+                    </div>
+
                 </div>
+
             </div>
             <div className={styles.tutorialPanel}>
                 <div className={styles.tutorialText} >
+                    <div>
+                        <h1 style={{ fontSize: "60px", fontWeight: "700" }}>Determine a timeline</h1>
+                        <h2 style={{ fontSize: "40px" }}>Set milestones that will be achieved over the course of the fundraiser.</h2>
+                    </div>
 
                 </div>
                 <div style={{
-                    textAlign: "center", width: "75%", backgroundColor: "yellow"
+                    textAlign: "center", width: "50%"
                 }}>
-                    <Image src="/../public/info-images/idea/contract.png" height="800" width="800" alt="image"></Image>
+                    <Image src="/../public/info-images/idea/idea.png" height="800" width="800" alt="image"></Image>
                 </div>
+
 
             </div>
             <div className={styles.tutorialPanel}>
