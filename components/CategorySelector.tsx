becf6f825242de1a1bda0a70b043d896afd456ff
Dylan <dhp3190@gmail.com>
2022-12-15 19:57:22 -0500

    hovering of categories

--- components/CategorySelector.tsx
+++ components/CategorySelector.tsx
@@ -35,7 +35,7 @@ export default function CategorySelector(props: { onChangeCategory?(arg0: void):
                                     <a
 
                                         className={classNames(
-                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            active ? 'bg-gray-100 text-gray-900 hover:text-indigo-900 hover:bg-stone-100' : 'text-gray-700 hover:text-indigo-900 hover:bg-stone-100',
                                             'block px-4 py-2 text-lg'
                                         )} onClick={(e) => { if (props.onChangeCategory) { props.onChangeCategory() } }}
                                     >
