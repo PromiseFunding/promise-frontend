24b249962f09609757c0c8b5391c68a621619dd8
Dylan <dhp3190@gmail.com>
2022-12-23 12:24:47 -0500

    added background circles stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
 import type { AppProps } from "next/app"
-import { Poppins } from '@next/font/google'
-const { library, config } = require('@fortawesome/fontawesome-svg-core');
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCoffee, fas } from '@fortawesome/free-solid-svg-icons'
+import { Poppins } from "@next/font/google"
+const { library, config } = require("@fortawesome/fontawesome-svg-core")
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { faCoffee, fas } from "@fortawesome/free-solid-svg-icons"
+import Particles from "react-tsparticles"
+import { loadFull } from "tsparticles"
+import { useCallback } from "react"
+import { Engine } from "tsparticles-engine"
+
 
 library.add(fab, faCoffee, fas)
 
@@ -13,16 +18,57 @@ const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || ""
 const APP_ID = process.env.NEXT_PUBLIC_APP_ID || ""
 
 const poppins = Poppins({
-    weight: ['400', '700'],
-    style: ['normal', 'italic'],
-    subsets: ['latin'],
+    weight: ["400", "700"],
+    style: ["normal", "italic"],
+    subsets: ["latin"],
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
+    let page = "" //for now
+    const options = {
+        // background: {
+        //     color: "#fff",
+        // },
+        particles: {
+            shape: {
+                type: "circle",
+            },
+            number: {
+                value: 7,
+            },
+            color: {
+                // value: "random",
+                //value: ["#0A1A6A", "#A42525"]
+                value: "#A42525",
+            },
+            opacity: {
+                value: 0.3,
+            },
+            size: {
+                value: { min: 15, max: 30 },
+            },
+            move: {
+                enable: true,
+                speed: 0.25,
+                random: false,
+            },
+            bounds: {
+                top: 500,
+            },
+        },
+    }
+
+    const particlesInit = useCallback(async (engine: Engine) => {
+        await loadFull(engine)
+    }, [])
+
     return (
         <main className={poppins.className}>
             <MoralisProvider initializeOnMount={false}>
                 <NotificationProvider>
+                    {!page.includes("form") ? (
+                        <Particles options={options} init={particlesInit} style={{ zIndex: -1 }} />
+                    ) : null}
                     <Component {...pageProps} />
                 </NotificationProvider>
             </MoralisProvider>
